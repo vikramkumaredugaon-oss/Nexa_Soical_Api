a1040babeb049fd8d7aa55a45db97c9c1a14b1d6
Vikram Kumar <vikramkumar.edugaon@gmail.com>
2026-07-24 13:39:29 +0530

    fix AuthController filename case

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const AuthController = require("../controllers/authController");
+const AuthController = require("../controllers/AuthController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 
