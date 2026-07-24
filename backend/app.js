80f6a7a449e386ea877e79197b26aad6ac5898cd
Vikram Kumar <vikramkumar.edugaon@gmail.com>
2026-07-24 12:59:10 +0530

    fix AuthController filename case

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 
-require("./config/database");
-
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const otpRoutes = require("./routes/otpRoutes");
 
 const app = express();
 
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
 
-// Routes
+// ROUTES
 app.use("/api/auth", authRoutes);
 
 app.use("/api/otp", otpRoutes);
@@ -23,14 +19,11 @@ app.use("/api/otp", otpRoutes);
 app.use("/api/profile", profileRoutes);
 
 
-// Test API
-app.get("/", (req, res) => {
-
+app.get("/", (req,res)=>{
     res.json({
-        success: true,
-        message: "Nexa Social Backend Running..."
+        success:true,
+        message:"Nexa Social Backend Running"
     });
-
 });
 
 
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const AuthController = require("../controllers/AuthController");
+const AuthController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 
